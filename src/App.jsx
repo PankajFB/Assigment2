8a64470b31892e40c56b4e4e11b246aec3d06171
Pankaj Kumar <85155188+PankajFB@users.noreply.github.com>
2024-03-28 01:36:44 +0530

    stated my project in vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container mx-auto px-4 py-8 bg-gray-100 shadow-lg rounded-lg">
+    <div className="text text-center">
+      <h1 className="text-3xl font-bold tracking-wide text-blue-700">Hi, this is my project in Vite</h1>
+    </div>
+  </div>
+    </>
+  )
+}
+
+export default App
